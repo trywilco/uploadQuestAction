b91671aa9b36886b59c7d291ee19e6a3019dafcb
Dar Malovani <dar.malovani@wilcohq.com>
2023-02-07 13:21:00 +0200

    feat: add `only-validate` param to validate quests on prs

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ const zipQuest = async () => {
   return zipFile;
 }
 
-const uploadQuest = async (questId, zipFile) => {
-  console.log(`Uploading ${questId}`);
+const uploadQuest = async (questId, zipFile, onlyValidate = false) => {
   const body = new FormData();
   body.append('file', fs.readFileSync(zipFile), {
     contentType: 'application/x-zip-compressed',
@@ -28,32 +27,55 @@ const uploadQuest = async (questId, zipFile) => {
     filename: zipFile,
   });
 
-  const url = `${core.getInput('wilco-engine-url')}/api/v1/editor/quest/${questId}?isPrimaryId=true`
-    const res = await fetch(url, { 
-      method: 'PUT', 
-      headers: {
-        "x-editor-user-token": core.getInput('quest-editor-user-token'),
-        "x-editor-user-email": core.getInput('quest-editor-user-email'),
-      },
-      body,
-    });
-        
-    if (!res.ok) {
-      console.log("Uploading failed");
-      const resJson = await res.json();
-      console.log(resJson.error?.message);
-      throw new Error(resJson.message);
-    } else {
-      console.log("Uploading succeeded")
-      console.log(await res.json());
-    }
+  const url = `${core.getInput('wilco-engine-url')}/api/v1/editor/quest/${questId}?isPrimaryId=true&onlyValidate=${onlyValidate}`
+  return await fetch(url, {
+    method: 'PUT',
+    headers: {
+      "x-editor-user-token": core.getInput('quest-editor-user-token'),
+      "x-editor-user-email": core.getInput('quest-editor-user-email'),
+    },
+    body,
+  });
+}
+
+const createDraft = async (questId, zipFile) => {
+  console.log(`Uploading ${questId}`);
+  const res = await uploadQuest(questId, zipFile);
+  if (!res.ok) {
+    console.log("Failed to create draft");
+    const resJson = await res.json();
+    console.log(resJson.error?.message);
+    throw new Error(resJson.message);
+  } else {
+    console.log("Draft was created successfully")
+    console.log(await res.json());
+  }
+}
+
+const validateQuest = async (questId, zipFile) => {
+  console.log(`Uploading ${questId}`);
+  const res = await uploadQuest(questId, zipFile, true);
+  if (!res.ok) {
+    console.log("Failed to validate quest");
+    const resJson = await res.json();
+    console.log(resJson.error?.message);
+    throw new Error(resJson.message);
+  } else {
+    console.log("Quest was validated successfully")
+    console.log(await res.json());
+  }
 }
 
 const main = async () => {
   try {
     const questId = await getQuestId();
     const zipFile = await zipQuest();
-    await uploadQuest(questId, zipFile);
+    const onlyValidate = core.getInput('only-validate');
+    if (onlyValidate) {
+      await validateQuest(questId, zipFile);
+    } else {
+      await createDraft(questId, zipFile);
+    }
   } catch (error) {
     core.setFailed(error.message);
   }
